401b21eba2a6fcc58af1f18246f25d4c8066e09a
Yann Pichereau <yann_pichereau@hotmail.fr>
2021-06-30 22:36:11 +0200

    Updates

---
+++ precache-manifest.2784419234fb160fd5de802e084b8905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f428936c796a7dc331519b6f4154fc8",
+    "url": "/VotingSystem_dApp/index.html"
+  },
+  {
+    "revision": "3dd19f91823662599c22",
+    "url": "/VotingSystem_dApp/static/css/main.553e4e85.chunk.css"
+  },
+  {
+    "revision": "6fd142fed8c61a215d19",
+    "url": "/VotingSystem_dApp/static/js/2.d7a62ebf.chunk.js"
+  },
+  {
+    "revision": "3dd19f91823662599c22",
+    "url": "/VotingSystem_dApp/static/js/main.2703174a.chunk.js"
+  },
+  {
+    "revision": "3566dc5b393df50c94df",
+    "url": "/VotingSystem_dApp/static/js/runtime-main.15278403.js"
+  }
+]);
